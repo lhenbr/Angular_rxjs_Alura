ac66a00462c75f1684181875c485a57e501673c1
Lucas <lucashenbr@gmail.com>
2021-06-06 12:16:43 -0300

    primeiro commit

---
+++ byte-bank-broker/src/app/acoes/acoes.service.ts
@@ -0,0 +1,32 @@
+import { Acoes, Acao, AcoesApi } from './modelo/acoes';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map, pluck, tap } from 'rxjs/operators';
+import { returnPoI18nService } from '@po-ui/ng-components';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AcoesService {
+
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+  getAcoes() {
+    return this.httpClient.get<AcoesApi>('http://localhost:3000/acoes')
+      .pipe(
+        pluck('payload'),
+        map((acoes =>
+          acoes.sort((acaoA, acaoB) => this.ordenaPorCodigo(acaoA, acaoB)
+    ))));
+  }
+  private ordenaPorCodigo(acaoA: Acao, acaoB: Acao){
+    if (acaoA.codigo > acaoB.codigo){
+      return 1;
+    }
+    if (acaoA.codigo < acaoB.codigo){
+      return -1;
+    }
+    return 0;
+  }
+}
