47d56e09d3d1e151ee646796d34aaf991f12d971
Lucas <lucashenbr@gmail.com>
2021-06-07 20:11:14 -0300

    finalizando projeto

--- byte-bank-broker/src/app/acoes/acoes.component.ts
+++ byte-bank-broker/src/app/acoes/acoes.component.ts
@@ -1,19 +1,31 @@
-import { Observable, Subscription } from 'rxjs';
+import { merge, Observable, Subscription } from 'rxjs';
 import { AcoesService } from './acoes.service';
-import { Acoes } from './modelo/acoes';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormControl } from '@angular/forms';
-
+import { debounceTime, distinctUntilChanged, filter, switchMap, tap } from 'rxjs/operators';
+const ESPERA_DIGITAÇAO = 300;
 @Component({
   selector: 'app-acoes',
   templateUrl: './acoes.component.html',
   styleUrls: ['./acoes.component.css'],
 })
 export class AcoesComponent {
+
   acoesInput = new FormControl();
+  todasAcoes$ = this.acoesService.getAcoes().pipe(tap(() => { console.log('fluco inicial'); }));
+  filtroPeloInput$ = this.acoesInput.valueChanges.pipe(
+    debounceTime(ESPERA_DIGITAÇAO),
+    tap(() => {
+      console.log('fuxo do filtro');
+    }),
+    tap(console.log),
+    filter(valorDigitado => valorDigitado.length >= 3 || !valorDigitado.length),
+    distinctUntilChanged(),
+    tap(console.log),
+    switchMap(
+      valordigitado => this.acoesService.getAcoes((valordigitado))));
 
-  acoes$ = this.acoesService.getAcoes();
-
+  acoes$ = merge(this.todasAcoes$, this.filtroPeloInput$);
 
   constructor(
     private acoesService: AcoesService
--- byte-bank-broker/src/app/acoes/acoes.service.ts
+++ byte-bank-broker/src/app/acoes/acoes.service.ts
@@ -1,5 +1,5 @@
 import { Acoes, Acao, AcoesApi } from './modelo/acoes';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map, pluck, tap } from 'rxjs/operators';
 import { returnPoI18nService } from '@po-ui/ng-components';
@@ -12,19 +12,20 @@ export class AcoesService {
   constructor(
     private httpClient: HttpClient
   ) { }
-  getAcoes() {
-    return this.httpClient.get<AcoesApi>('http://localhost:3000/acoes')
+  getAcoes(valor?: string) {
+    const params = valor ? new HttpParams().append('valor', valor) : undefined;
+    return this.httpClient.get<AcoesApi>('http://localhost:3000/acoes', { params })
       .pipe(
         pluck('payload'),
         map((acoes =>
           acoes.sort((acaoA, acaoB) => this.ordenaPorCodigo(acaoA, acaoB)
-    ))));
+          ))));
   }
-  private ordenaPorCodigo(acaoA: Acao, acaoB: Acao){
-    if (acaoA.codigo > acaoB.codigo){
+  private ordenaPorCodigo(acaoA: Acao, acaoB: Acao) {
+    if (acaoA.codigo > acaoB.codigo) {
       return 1;
     }
-    if (acaoA.codigo < acaoB.codigo){
+    if (acaoA.codigo < acaoB.codigo) {
       return -1;
     }
     return 0;
